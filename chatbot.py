c1e3424735105005b098758c3cead28b3add9983
Lucas Rampim <lucas.rampim@outlook.com>
2018-07-16 21:25:54 -0300

    first commit

---
+++ chatbot.py
@@ -0,0 +1,13 @@
+# Building a Chatbot with Deep NLP
+
+
+
+# Importing the libraries
+import numpy as np
+import tensorflow as tf
+import re
+import time
+
+
+
+################ PART 1 - DATA PREPROCESSING ################
